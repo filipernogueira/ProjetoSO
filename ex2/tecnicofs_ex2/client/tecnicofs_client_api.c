7574feaeebc601730744157793b9d246d7f8b8a0
Completo <completo22@gmail.com>
2022-01-30 16:29:02 +0000

    nsei

--- ex2/tecnicofs_ex2/client/tecnicofs_client_api.c
+++ ex2/tecnicofs_ex2/client/tecnicofs_client_api.c
@@ -24,7 +24,7 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
         return -1;
     }
     char buffer[BUFFER_SIZE];
-    tx = tfs_open(server_pipe_path, O_WRONLY);
+    tx = open(server_pipe_path, O_WRONLY);
     if (tx == -1) {
         fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
         return -1;
@@ -34,67 +34,64 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     memcpy(buffer, &opcode, sizeof(char));
     memcpy(buffer + 1, client_pipe_path, sizeof(char) * strlen(client_pipe_path));
 
-    if (tfs_write(tx, buffer, BUFFER_SIZE) == -1){
+    if (write(tx, buffer, BUFFER_SIZE * sizeof(char)) == -1){
         fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
         return -1;
     }
 
-    rx = tfs_open(client_pipe_path, O_RDONLY);
-    if (tx == -1) {
+    rx = open(client_pipe_path, O_RDONLY);
+    if (rx == -1) {
         fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
         return -1;
     }  
 
-    int ret;
+    int ret = -1;
 
-    if(tfs_read(tx, ret, sizeof(int)) == -1){
+    if(read(rx, &ret, sizeof(int)) == -1 || ret < 0){
         fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));
+        return -1;
     }
 
-    session_id++;
+    session_id = ret;
 
     return 0;
 }
 
 int tfs_unmount() {
-    
     int const opcode = TFS_OP_CODE_UNMOUNT;
     int buffer[2];
 
     buffer[0] = opcode;
     buffer[1] = session_id;
 
-    if(tfs_write(tx, buffer, 2) == -1)
+    if(write(tx, buffer, 2 * sizeof(int)) == -1){
+        fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
         return -1;
+    }
     
-    if(tfs_close(tx) == -1)
+    if(close(tx) == -1){
+        fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));
         return -1;
+    }
 
-    if(tfs_close(rx) == -1)
+    if(close(rx) == -1){
+        fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));
         return -1;
+    }
 
     return 0;
 }
 
 int tfs_open(char const *name, int flags) {
-    if(open(name, flags) == -1)
-        return -1;
-
-    return 0;
+    
 }
 
 int tfs_close(int fhandle) {
-    if(close(fhandle) == -1)
-        return -1;
-
-    return 0;
+    
 }
 
 ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
-    if(write(fhandle, buffer, len) == -1);
-        return -1;
     
-    return 0;
 }
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
--- ex2/tecnicofs_ex2/fs/tfs_server.c
+++ ex2/tecnicofs_ex2/fs/tfs_server.c
@@ -11,9 +11,65 @@
 #include <unistd.h>
 
 int session_ids[S];
+int static rx;
+int open_sessions = 0;
+
+int server_mount(){
+    char client_pipe_path[40];
+
+    if(read(rx, &client_pipe_path, sizeof(char) * 40) == -1){
+        fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    int tx = open(client_pipe_path, O_WRONLY);
+    if (tx == -1) {
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    if(open_sessions >= S){
+        int error = -1;
+        write(tx, &error, sizeof(int));
+        return -1;
+    }
+
+    int id = 0;
+    for(;id < S; id++){
+        if(session_ids[id] == -1){
+            session_ids[id] = tx;
+            open_sessions++;
+        }
+    }
+
+    if (write(tx, &id, sizeof(int)) == -1){
+        fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+int server_unmount(){
+    int id;
+
+    if(read(rx, &id, sizeof(int)) == -1){
+        fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    if(close(session_ids[id]) == -1){
+        fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));
+        return -1;
+    }
+    
+    session_ids[id] == -1;
+    open_sessions--;
+    
+    return 0;
+}
 
 int main(int argc, char **argv) {
-
     if (argc < 2) {
         printf("Please specify the pathname of the server's pipe.\n");
         return 1;
@@ -30,6 +86,53 @@ int main(int argc, char **argv) {
     if (mkfifo(pipename, 0640) != 0) {
         fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < S; i++)
+        session_ids[i] = -1;
+
+    rx = open(pipename, O_RDONLY);
+
+    if(rx == -1){
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    while(true){
+        char op_code;
+        int ret = read(rx, &op_code, sizeof(char));
+
+        if(ret == -1){
+            fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));
+            return -1;
+        } else if (ret == 0){
+            continue;
+        }
+        switch(op_code){
+            case TFS_OP_CODE_MOUNT:
+                server_mount();
+                break;
+            case TFS_OP_CODE_UNMOUNT:
+                server_unmount();
+                break;
+            case TFS_OP_CODE_OPEN:
+                server_open();
+                break;
+            case TFS_OP_CODE_CLOSE:
+                server_close();
+                break;
+            case TFS_OP_CODE_WRITE:
+                server_write();
+                break;
+            case TFS_OP_CODE_READ:
+                server_read();
+                break;
+            case TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED:
+                server_shutdown();
+                break;
+            default:
+                break;
+        }
     }
 
 
